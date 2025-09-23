2bc27bafaf6b0853e9bde9d18cb6d5a286f5cc4c
Maria Luiza Baborsa <marialuiza.bf.silva@gmail.com>
2025-09-23 11:58:19 -0300

    feat: introduzindo middleware

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,6 +6,14 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    
+    public function index()
+    {
+        echo "I am inside the app!";
+    }
+
+    public function newNote()
+    {
+        echo "I am creating a new note!";
+    }
 }
 
