9a08633fff6ff1d51f9aaa5ad60e8cd4a17e200d
Maria Luiza Baborsa <marialuiza.bf.silva@gmail.com>
2025-09-26 10:50:47 -0300

    refactor: construindo o Services e melhorando a distribuição da function "decryptId"

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Services\Operations;
 use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
@@ -27,25 +28,16 @@ class MainController extends Controller
 
     public function editNote($id)
     {
-        $id = $this->decryptId($id);
+        // $id = $this->decryptId($id);
+        $id = Operations::decryptId($id);
         echo "Eu estou editando a note com o id = $id";
     }
 
     public function deleteNote($id)
     {
-        $id = $this->decryptId($id);
+        // $id = $this->decryptId($id);
+        $id = Operations::decryptId($id);
         echo "Eu estou deletando a note com o id = $id";
-    }
-
-    private function decryptId($id)
-    {
-        try {
-            $id = Crypt::decrypt($id);
-        } catch (DecryptException $e) {
-            return redirect()->route('home');
-        
-    }
-    return $id;
     }
 }
 
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::middleware([CheckIsLogged::class])->group(function(){
     Route::get('/newNote', [MainController::class, 'newNote'])->name('new');
 
     Route::get('/editNote/{id}', [MainController::class, 'editNote'])->name('edit');
-    Route::get('/deleteNote/{id}', [MainController::class, 'editNote'])->name('delete');
+    Route::get('/deleteNote/{id}', [MainController::class, 'deleteNote'])->name('delete');
 
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 });
